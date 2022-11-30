e5adb60b519ad6eee5d515e29e7382f8a76ab0f2
MonteRyan61 <ryan.monteleone@colorado.edu>
2022-11-30 12:18:59 -0700

    Minor change to main container

--- cook-book/src/MainContainer.js
+++ cook-book/src/MainContainer.js
@@ -2,9 +2,6 @@ import React from "react";
 import './MainContainer.css';
 import Search from "./Search";
 import CookingTitles from "./CookingTitles";
-// import Flex from "./flexbox";
-
-
 export default class form extends React.Component{
     constructor(props){
         super(props);
@@ -19,12 +16,7 @@ export default class form extends React.Component{
             <div className="Main-header">
                 <h1>{this.state.Header}</h1>
                 <Search />
-                <CookingTitles/>
-
-                {/* <div className= "Gallery">
-                    <flexbox/>
-                
-                </div> */}
+                <CookingTitles />
             </div>
         );
     }
